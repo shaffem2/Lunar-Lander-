ed4cef557f5b4b44fcce9cd4ad9e40e7f3e58190
William Heeb <heebw@sunypoly.edu>
2019-02-19 12:18:36 -0500

    Basics

---
+++ Assets/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public Rigidbody rigidShip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Vertical") > 0.01f)
+        {
+            rigidShip.AddForce(transform.up * 20.0f);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.01f || Input.GetAxis("Horizontal") < 0.01f)
+        {
+            rigidShip.AddForce((transform.right * 6.0f) * Input.GetAxis("Horizontal"));
+        }
+    }
+}
