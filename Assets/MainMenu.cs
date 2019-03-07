df65242bba9520bb2f5e6636ba074e8652683d1f
William Heeb <heebw@sunypoly.edu>
2019-03-06 23:21:15 -0500

    Main Menu
    Added: Main menu screen with play, options, and and exit button. Added options screen with volume slider and back button.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        // Grabs the first scene in the queue to start game (Check: FILE >> BUILD SETTINGS for queue)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame() //
+    {
+        Debug.Log("GAME HAS CLOSED!");  // Debugger for Unity testing only to show that the game would exit normally
+        Application.Quit();             // Exits game
+    }
+}
