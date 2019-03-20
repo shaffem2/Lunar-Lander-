5cd9c9206ca6f0d56a551f85057179916e8595a4
William Heeb <heebw@sunypoly.edu>
2019-03-19 22:37:00 -0400

    Fixed Double Script
    Wall script component was added twice to wall.

--- Assets/Textures/wall.cs
+++ Assets/Textures/wall.cs
@@ -11,11 +11,10 @@ public class wall : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > .001f && shouldCheckForCollision)
+        if (collision.relativeVelocity.magnitude > 0f && shouldCheckForCollision)
         {
             StartCoroutine(GameObject.Find("PlayerLives").GetComponent<PlayerLives>().TakeLife());  // Calls function to take a life away
             StartCoroutine(GameObject.Find("GameManager").GetComponent<GameManager>().Restart());   // Calls level restart function
-            
             Destroy(collision.gameObject);
             Debug.Log("You have CRASHED!");
         }
