09b40811f6c93c8ea1d03c6cab533cec22fd4e35
devonnguyen98 <47196117+devonnguyen98@users.noreply.github.com>
2019-03-21 02:40:30 -0400

    Particles
    Added three Prelabs:
    Bottom Thruster
    Left Thruster
    Right Thruster
    
    Attach Prelabs to Ship

--- Assets/Textures/Ship.cs
+++ Assets/Textures/Ship.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     public Rigidbody rigidShip;
+    public ParticleSystem BottomThruster;
+    public ParticleSystem LeftThruster;
+    public ParticleSystem RightThruster;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +26,42 @@ public class Ship : MonoBehaviour
         if (Input.GetAxis("Vertical") > 0.01f)
         {
             rigidShip.AddForce(transform.up * 10.0f);
+
+            if(!BottomThruster.isPlaying)
+            {
+                BottomThruster.Play();
+            }
         }
 
-        if (Input.GetAxis("Horizontal") > 0.01f || Input.GetAxis("Horizontal") < 0.01f)
+        else if (BottomThruster.isPlaying)
+        {
+            BottomThruster.Stop();
+        }
+
+
+        if (Input.GetAxis("Horizontal") > 0.01f || Input.GetAxis("Horizontal") < -0.01f)
         {
             rigidShip.AddForce((transform.right * 6.0f) * Input.GetAxis("Horizontal"));
+
+            if (!LeftThruster.isPlaying && Input.GetAxis("Horizontal") > 0.01f)
+            {
+                LeftThruster.Play();
+            }
+
+            else if(!RightThruster.isPlaying && Input.GetAxis("Horizontal") < -0.01f)
+            {
+                RightThruster.Play();
+            }
+        }
+
+        if (LeftThruster.isPlaying && Input.GetAxis("Horizontal") < 0.01f)
+        {
+            LeftThruster.Stop();
+        }
+
+        else if (RightThruster.isPlaying && Input.GetAxis("Horizontal") > -0.01f)
+        {
+            RightThruster.Stop();
         }
     }
 }
