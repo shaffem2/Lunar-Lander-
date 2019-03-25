db783d3b56c7c59da36c8837911bd48b0e83f290
Max Shaffer <max.r.shaffer@gmail.com>
2019-03-24 22:40:13 -0400

    wall fix pt 2

---
+++ Assets/Textures/Wall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public bool isStartingPlatform = false;
+    public Collider collider;
+    bool shouldCheckForCollision = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 0f && shouldCheckForCollision)
+        {
+            StartCoroutine(GameObject.Find("PlayerLives").GetComponent<PlayerLives>().TakeLife());  // Calls function to take a life away
+            StartCoroutine(GameObject.Find("GameManager").GetComponent<GameManager>().Restart());   // Calls level restart function
+            Destroy(collision.gameObject);
+            Debug.Log("You have CRASHED!");
+        }
+    }
+
+
+}
