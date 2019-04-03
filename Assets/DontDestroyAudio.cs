25627cdfb28da5641230042f8ab3fe76151563a8
William Heeb <heebw@sunypoly.edu>
2019-04-03 02:15:24 -0400

    Game Audio/Options Page Update
    Fixed options menu screen resolution scaling.  Added background music across all scenes. Added functionality for volume slider on options menu.

---
+++ Assets/DontDestroyAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour
+{
+    private static DontDestroyAudio instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float sliderValue)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(sliderValue) * 20);
+    }
+}
