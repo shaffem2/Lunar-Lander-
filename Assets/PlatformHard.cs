87e504e57d2ff17ca38dd15c764a7c2e61399374
Max Shaffer <max.r.shaffer@gmail.com>
2019-04-09 19:56:54 -0400

    updated hard platform script with fix

--- Assets/PlatformHard.cs
+++ Assets/PlatformHard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlatformEasy : MonoBehaviour
+public class PlatformHard : MonoBehaviour
 {
     public Collider collider;
     bool shouldCheckForCollision = true;
