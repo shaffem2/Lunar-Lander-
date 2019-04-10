5f689ecce2e677d33ee2adbdff62f98891245043
devonnguyen98 <47196117+devonnguyen98@users.noreply.github.com>
2019-04-10 16:13:53 -0400

    Prefab LandPlatform

---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoretext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //scoreText.text = 
+    }
+}
