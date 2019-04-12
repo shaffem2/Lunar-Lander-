c29d914bc0248431a1024e1c25d51b248b1b9614
devonnguyen98 <47196117+devonnguyen98@users.noreply.github.com>
2019-04-11 21:10:07 -0400

    Fuel UI Added

---
+++ Assets/Fuel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuel : MonoBehaviour
+{
+    public static int fuelValue;
+    Text fuel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuel = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuel.text = "Score: " + fuelValue; //Displays Fuel
+    }
+}
+
