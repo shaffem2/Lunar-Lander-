ae82fc9b19f2f49891aa9202985b3d9e73519eab
Max Shaffer <max.r.shaffer@gmail.com>
2019-04-15 00:00:45 -0400

    Added fuel powerup, edited most levels to use powerup

---
+++ Assets/FuelPowerup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelPowerup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Ship").GetComponent<Ship>().SetFuel(1);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -14,12 +14,18 @@ public class Ship : MonoBehaviour
     public AudioClip thrusterAudio;
     public Slider fuelBar;
 
+    public void SetFuel(float value) //fucntion to set the fuel level mid game, used by fuel powerup
+    {
+        GameManager.fuel = value;
+        fuelBar.value = value;
+        return;
+    }
+
     void Awake()
     {
         source = GetComponent<AudioSource>();
         source.clip = thrusterAudio;
-        GameManager.fuel = 1.0f;
-        fuelBar.value = GameManager.fuel;
+        SetFuel(1);
     }
     
     public IEnumerator ShipGoBoom()  //this function destroys the ship with an explosion effect
@@ -39,8 +45,8 @@ public class Ship : MonoBehaviour
     {
         if ((Input.GetAxis("Vertical") > 0.05f) && GameManager.fuel > 0)
         {
-            rigidShip.AddForce(transform.up * 10.0f);
-            GameManager.fuel += -0.1f * Time.deltaTime;
+            rigidShip.AddForce(transform.up * 10.0f);   //move ship
+            GameManager.fuel += -0.1f * Time.deltaTime; //fuel burn
             fuelBar.value = GameManager.fuel;
 
             if(!BottomThruster.isPlaying)
@@ -57,8 +63,8 @@ public class Ship : MonoBehaviour
 
         if ((Input.GetAxis("Horizontal") > 0.01f || Input.GetAxis("Horizontal") < -0.01f) && GameManager.fuel > 0)
         {
-            rigidShip.AddForce((transform.right * 6.0f) * Input.GetAxis("Horizontal"));
-            GameManager.fuel += -0.1f * Time.deltaTime;
+            rigidShip.AddForce((transform.right * 6.0f) * Input.GetAxis("Horizontal")); //move ship
+            GameManager.fuel += -0.1f * Time.deltaTime; //fuel burn
             fuelBar.value = GameManager.fuel;
 
             if (!LeftThruster.isPlaying && Input.GetAxis("Horizontal") > 0.01f)
